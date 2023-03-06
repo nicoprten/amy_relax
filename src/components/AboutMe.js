537ada21f573f9ffec150c79a06c359e388526fd
admin <admin@MBP-de-admin.fibertel.com.ar>
2023-03-06 17:30:37 -0300

    UI about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import { WhatsappLogo } from 'phosphor-react';
+import { Link } from 'react-router-dom';
+import { WhatsappLogo, InstagramLogo } from 'phosphor-react';
 
 
 export const AboutMe = () => {
   return (
-    <div className='flex justify-between gap-20 px-80 mt-[-100px]' id='sobre-mi'>
-        <div className='flex flex-col w-2/6'>
-          <h2 className='text-[32px] text-green font-extrabold leading-relaxed tracking-wide'>María Amalia Alvarez</h2>
-          <p className='text-lg text-green'>Masajista - Terapeuta Holística - Logopeda</p>
+    <div className='w-[70vw] mx-auto my-10 flex justify-between sm:flex-wrap-reverse' id='sobre-mi'>
+        <div className='flex flex-col xl:w-3/4 bg-brown rounded xl:pr-20 border-1 border-gray xl:bg-white xl:border-none'>
+          <p className='text-sm my-2 bg-green w-max p-2 rounded-t text-brown'>MASAJISTA | TERAPEUTA HOLÍSTICA | LOGOPEDA</p>
+          <h2 className='xl:text-5xl text-2xl text-green font-extrabold leading-relaxed tracking-wide'>María Amalia Alvarez</h2>
           <p className='text-lg mt-[30px]'>Especialista en las técnicas del masaje terapéutico y de las diferentes técnicas de masaje deportivo, según los distintos tipos de actividades físicas.</p>
-          <button className='flex gap-4 items-center w-max mt-4 py-2 px-4 bg-green text-white'>
-            <WhatsappLogo className='text-green bg-white rounded-full' size={34} />
-            Háblame
+          <button className='flex gap-4 items-center w-max mt-4 py-2 px-4 bg-white text-black border-2 border-black rounded hover:bg-black hover:text-white duration-200'>
+            Reserva tu turno
           </button>
         </div>
-        <img className='w-[400px] h-[300px] object-cover' src='./img/amalia_profile.jpg' alt='Foto de perfil de Amalia' />
+        <figure className='relative h-max xl:w-1/4'>
+          <img className='w-[300px] h-[300px] object-cover rounded border-1 border-gray shadow-lg' src='./img/amalia_profile.jpg' alt='Foto de perfil de Amalia' />
+          <figcaption className='absolute flex bg-black bottom-0 text-white0 w-full rounded-t'>
+            <Link className='w-1/2 flex items-center justify-center gap-2 p-2 rounded-t hover:bg-white0 hover:text-black duration-200' to=''>
+              <WhatsappLogo size={22} />
+              <p className='text-xs'>whatsapp</p>
+            </Link>
+            <Link className='w-1/2 flex items-center justify-center gap-2 p-2 rounded-t hover:bg-white0 hover:text-black duration-200' to='https://www.instagram.com/amy_alvarez_17/' target='_blank'>
+              <InstagramLogo size={22} />
+              <p className='text-xs'>instagram</p>
+            </Link>
+          </figcaption>
+        </figure>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,17 @@ import React from 'react';
 
 export const NavBar = () => {
 
-    function scrollLink(e){
-        let target = e.target.innerHTML.toLowerCase();
-        let targetFixed = target.replace(' ', '-');
-        targetFixed = targetFixed === 'logo' ? 'home' : targetFixed;
-        // console.log(newTarget);
-        document.getElementById(targetFixed).scrollIntoView();
-    }
+    // function scrollLink(e){
+    //     let target = e.target.innerHTML.toLowerCase();
+    //     let targetFixed = target.replace(' ', '-');
+    //     targetFixed = targetFixed === 'logo' ? 'home' : targetFixed;
+    //     document.getElementById(targetFixed).scrollIntoView();
+    // }
 
   return (
-    <div className='flex justify-between bg-brown py-4 px-80 h-[20vh]'>
-        <p onClick={(e) => scrollLink(e)}>LOGO</p>
-        <ul className='flex gap-20 list-none text-green'>
+    <div className='flex justify-center bg-brown p-4'>
+        <p className='absolute left-0 pl-2' onClick={(e) => scrollLink(e)}>LOGO</p>
+        <ul className='flex gap-10 list-none text-green'>
             <li><button className='border-b-2 border-brown hover:border-green duration-200' onClick={(e) => scrollLink(e)}>SOBRE MI</button></li>
             <li><button className='border-b-2 border-brown hover:border-green duration-200' onClick={(e) => scrollLink(e)}>MASAJES</button></li>
             <li><button className='border-b-2 border-brown hover:border-green duration-200' onClick={(e) => scrollLink(e)}>TURNOS</button></li>
