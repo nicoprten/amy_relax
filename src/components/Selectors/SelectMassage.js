03c6c8037b8f4fa8e9b7ed17b6c5bfff46cd48e3
nicoprten <nicoprtenjaca@gmail.com>
2023-03-22 01:43:01 -0300

    Improving the code in the SelectMassage component

--- src/components/Selectors/SelectMassage.js
+++ src/components/Selectors/SelectMassage.js
@@ -9,13 +9,12 @@ export const SelectMassage = () => {
     const [massages, setMassages] = useState([])
 
     const reservation = useSelector(state => state.reservation)
-    
-    const getMassagesNow = async () => {
-        setMassages(await getMassages())
-    }
 
     useEffect(() => {
-        getMassagesNow();
+        (async () => {
+            setMassages(await getMassages())
+        })()
+        // WHY THIS?: https://ultimatecourses.com/blog/using-async-await-inside-react-use-effect-hook#:~:text=Why%3F,function%20will%20never%20get%20called.
     }, [])
 
   return (
@@ -25,7 +24,7 @@ export const SelectMassage = () => {
             <div className='flex flex-wrap justify-center gap-4 my-8 w-[60vw] mx-auto'>
                 { massages.length > 0 &&
                     massages.map((m, i) => 
-                        <button className='h-full w-[220px] text-center bg-black text-white p-2 rounded hover:shadow-xl duration-200' onClick={(e) => setReservation({...reservation, massage_type: e.target.innerHTML})}>{m.type}</button>
+                        <button className='h-full w-[220px] text-center bg-black text-white p-2 rounded hover:shadow-xl duration-200' onClick={(e) => setReservation({...reservation, massage_type: e.target.innerHTML})} key={i}>{m.type}</button>
                     )
                 }
             </div>
