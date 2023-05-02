30097befdc90ee09c5549e2e31b6f3e566945fc0
nicoprten <nicoprtenjaca@gmail.com>
2023-05-02 19:28:54 -0300

    redesign ui information

--- src/components/Information.js
+++ src/components/Information.js
@@ -12,7 +12,7 @@ export const Information = () =>{
     useEffect(() => {
         (async () => {
             const allMassages = await getMassages()
-            allMassages.map(m => setReadMore((prevState) => [...prevState, 100]))
+            allMassages.map(m => setReadMore((prevState) => [...prevState, 120]))
             setMassages(allMassages)
         })()
     }, [])
@@ -26,17 +26,17 @@ export const Information = () =>{
     return (
         <div className='bg-brown rounded-3xl'>
         {massages.length > 0 ?
-                <div className='w-[60vw] mx-auto py-10 flex flex-wrap justify-center gap-2'>
+                <div className='w-[80vw] mx-auto py-10 flex flex-wrap justify-start gap-2'>
                     {massages.map((m, i) => 
-                        <div className='relative bg-white0 w-[280px] h-max rounded-xl' key={i}>
-                            <img className='rounded-t-xl w-full h-[150px] object-cover' src={m.image} alt={m.type} />
-                            <div className='absolute top-2 right-2 flex flex-col gap-2 text-xs'>
+                        <div className='relative bg-white0 w-[300px] min-h-[140px] h-max rounded-l-xl border-r-[5px] border-brown-dark my-4' key={i}>
+                            {/* <img className='rounded-tl-xl w-full h-[150px] object-cover' src={m.image} alt={m.type} /> */}
+                            <div className='absolute top-2 right-[0px] flex flex-col gap-2 text-xs items-end'>
                                 {m.prices?.map((p,i) => 
                                     <p className='w-max bg-brown-dark text-white0 rounded-l p-1' key={i}>{p.duration}´ {p.price}€</p>    
                                 )}
                             </div>
                             <h3 className='relative w-max rounded bg-gray0 font-bold text-md mx-2 p-2 mt-[-20px]'>{m.type}</h3>
-                            <p className='text-sm p-2'>{m.description.slice(0, readMore[i])}{m.description.length > readMore[i] && <button className='text-xs text-violet-light' onClick={() => handleSeeMore(i, m.description.length)}> ...read more</button>}</p>
+                            <p className='text-sm p-2 pr-0 mr-[60px]'>{m.description.slice(0, readMore[i])}{m.description.length > readMore[i] && <button className='text-xs text-violet-light' onClick={() => handleSeeMore(i, m.description.length)}> ...read more</button>}</p>
                         </div>
                     )}
                 </div>
